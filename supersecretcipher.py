70ba17f8c1f0e7e9e23c1dfe1741981835c4288a
LewPort <porter.lewis@gmail.com>
2016-11-28 19:00:13 +0000

    Update supersecretcipher.py

--- supersecretcipher.py
+++ supersecretcipher.py
@@ -1,44 +1,59 @@
 import pyperclip
 import random
-alphabet = (r'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789,.!? ')
+import string
+
+alphabet = string.printable[0:95]
 alphabet = list(alphabet)
-key = 5
-codedmsg = []
-decodedmsg = []
 
 def encode(inputstring):
+    codedmsg = []
+    while True:
+        try:
+            key = int(input('\nSet a super secret key (Any number): '))
+            while key >= len(alphabet):
+                key = key // 2
+            break
+        except:
+            continue
     for i in inputstring:
         codedchar = (alphabet.index(i)) + key
-        if codedchar > (len(alphabet) - 1):
+        if codedchar >= (len(alphabet)):
             codedchar = codedchar - (len(alphabet))
         codedmsg.append(alphabet[codedchar])
     pyperclip.copy(''.join(codedmsg))
     print('\'' + ''.join(codedmsg) + '\' copied to clipboard.')
 
-def decode(codedmsg):
-    for i in codedmsg:
+def decode(inputstring):
+    decodedmsg = []
+    while True:
+        try:
+            key = int(input('\nSet a super secret key (Any number): '))
+            while key >= len(alphabet):
+                key = key // 2
+            break
+        except:
+            continue
+    for i in inputstring:
         decodedchar = (alphabet.index(i)) - key
         if decodedchar < 0:
             decodedchar = decodedchar + (len(alphabet))
         decodedmsg.append(alphabet[decodedchar])
     print(''.join(decodedmsg))
 
-while True:
-    while True:
-        try:
-            key = int(input('\nSet a super secret key (Any number): '))
-            while key > 40:
-                key = key // 2
-            break
-        except:
-            continue
-        
-    inputstring = input('Message: ')
-    inputstring = list(inputstring)
-    encode(inputstring)
-    decode(codedmsg)
-    codedmsg = []
+def brute(inputstring):
     decodedmsg = []
+    for i in range(len(alphabet)):
+        key = i
+        for i in inputstring:
+            decodedchar = (alphabet.index(i)) - key
+            if decodedchar < 0:
+                decodedchar = decodedchar + (len(alphabet))
+            decodedmsg.append(alphabet[decodedchar])
+        print(''.join(decodedmsg))
+        decodedmsg = []
+
+
+        
 
 
 # RANDOM KEY GENERATOR, cba to implement it though whatever
